59c91b48b74de7dc84c5893352bdbe672f0059df
vinhlt <vinh180902@gmail.com>
2024-09-18 14:13:05 +0700

    init proj

--- Source/Aura.Target.cs
+++ Source/Aura.Target.cs
@@ -8,8 +8,7 @@ public class AuraTarget : TargetRules
 	public AuraTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-
+		DefaultBuildSettings = BuildSettingsVersion.V4;
 		ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
 }
