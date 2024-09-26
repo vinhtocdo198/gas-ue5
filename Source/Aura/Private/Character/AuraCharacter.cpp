6393be98601506a41e31d613a9ac7e3fe69ce076
vinhlt <vinh180902@gmail.com>
2024-09-26 11:03:25 +0700

    set effect props

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -14,7 +14,7 @@ AAuraCharacter::AAuraCharacter()
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
 	GetCharacterMovement()->bConstrainToPlane = true;
-	GetCharacterMovement()->bSnapToPlaneAtStart = true;
+	GetCharacterMovement()->bSnapToPlaneAtStart = true; 
 
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
